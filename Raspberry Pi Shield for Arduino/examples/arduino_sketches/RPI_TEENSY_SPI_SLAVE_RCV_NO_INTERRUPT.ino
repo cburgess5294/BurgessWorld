609cc8dc49d5e059d4f41fae07c7b99379b8e727
cburgess5294 <flamingpinto@gmail.com>
2018-06-25 14:49:22 -0400

    adding arduino example sketches

---
+++ Raspberry Pi Shield for Arduino/examples/arduino_sketches/RPI_TEENSY_SPI_SLAVE_RCV_NO_INTERRUPT.ino
@@ -0,0 +1,53 @@
+
+#include <t3spi.h>
+
+//Initialize T3SPI class as SPI_SLAVE
+T3SPI SPI_SLAVE;
+
+//The number of integers per data packet
+//MUST be the same as defined on the MASTER device
+#define dataLength  5
+
+//Initialize the arrays for incoming data
+//volatile uint8_t data[dataLength] = {};
+volatile uint16_t data[dataLength] = {};
+
+
+
+uint8_t dataPointer = 0;
+void setup(){
+  Serial.begin(115200);
+  
+  //Begin SPI in SLAVE (SCK pin, MOSI pin, MISO pin, CS pin)
+  SPI_SLAVE.begin_SLAVE(SCK, MOSI, MISO, CS0);
+  
+  //Set the CTAR0_SLAVE0 (Frame Size, SPI Mode)
+  SPI_SLAVE.setCTAR_SLAVE(8, SPI_MODE0);
+  //SPI_SLAVE.setCTAR_SLAVE(16, SPI_MODE0);
+  SPI0_CTAR0 |= SPI_CTAR_DBR | SPI_CTAR_CSSCK(0b0111) | SPI_CTAR_BR(0b0111);//set SPI CLOCK SPEED TO 370Kbps
+ 
+}
+
+void loop(){
+  if (digitalRead(10) == 0){
+    while (dataPointer < dataLength){
+      delay(20);
+      while(!SPI_SR_RFDF){}//wait until byte is sent
+      data[dataPointer] = SPI0_POPR;
+      Serial.println(data[dataPointer],BIN);
+      dataPointer++;
+      SPI0_SR |= SPI_SR_RFDF;
+    }
+    if (dataPointer == dataLength){
+      dataPointer=0;
+    }
+    //Print data received
+    for (int i=0; i<dataLength; i++){
+      Serial.print("data[");
+      Serial.print(i);
+      Serial.print("]: ");
+      Serial.write(data[i]);
+      Serial.println();
+      Serial.flush();}   
+  }
+}
---
+++ Raspberry Pi Shield for Arduino/examples/arduino_sketches/RPi_ARDUINO_TEENSY_SERIAL_TEST.ino
@@ -0,0 +1,43 @@
+/* Test Sketch for the Raspberry Pi Shield for Arduino from BurgessWorld Custom Electronics
+ *  
+ * This sketch assumes that the sheild is being used with an Arduino-Teensy v4.x or v5.x board with
+ * a Teensy 3.1 or 3.2 installed and that the uC select jumpers are to the left-hand position
+ * (D8-TX / D7-RX).
+ * 
+ * The Serial port on the Raspberry Pi (/dev/ttyS0 or alias serial0) must be enabled using
+ * raspi-config or by modifying the appropriate configuration files.
+ * 
+ * You will also need to disable the Raspberry Pi console output to the serial port:
+ * $ sudo systemctl stop serial-getty@ttyS0.service
+ * $ sudo systemctl disable serial-getty@ttyS0.service
+ * and remove the refference to console=serial0,115200 from /boot/cmdline.txt
+ * 
+ * Verify that Python is installed and configured on the Raspberry Pi and that the example Python 
+ * scripts from the BurgessWorld Custom Electronics Github page are downloaded to your Pi.
+ * 
+ * This sketch, when used in conjunction with the python script on the Raspberry Pi, will
+ * wait for the Pi to issue a request for you to input some data via the Arduino IDE Serial Monitor
+ * then report back the data it received from you.  This will demonstrate bi-direction serial 
+ * communication between the Raspberry pi and Arduino or compatible Micro-controller
+ * 
+ * The SoftwareSerial port and Arduino IDE Serial monitor are configured for a BAUD rate of 57600
+ * 
+ */
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(57600);
+  Serial3.begin(57600);
+  Serial.println("rPI-Teensy Serial Test");
+}
+
+void loop() { // run over and over
+  if (Serial3.available() >= 1) {
+    Serial.write(Serial3.read());
+  }
+  if (Serial.available() >= 1) {
+    Serial3.write(Serial.read());
+  }
+  
+}
+
