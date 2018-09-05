384532392826cbb60a77e7c47efa87705d86cf41
Chris Burgess <flamingpinto@gmail.com>
2018-09-05 17:28:27 +0000

    Add rpi_avr_i2c_SMBUS_example_v2.py

---
+++ Raspberry Pi Shield for Arduino/examples/python_scrips/rpi_avr_i2c_SMBUS_example_v2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import smbus
+import time
+
+bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+
+# Give the I2C device time to settle
+time.sleep(2)
+
+DEVICE_ADDRESS = 0x8      #7 bit address (will be left shifted to add the read write bit)
+DEVICE_REG_MODE1 = 0x00
+DEVICE_REG_LEDOUT0 = 0x1d
+
+def StringToBytes(val):
+    retVal = []
+    for c in val:
+            retVal.append(ord(c))
+    return retVal
+
+byteString = ("hello")
+bytelist = StringToBytes(byteString)
+count = 0
+charBuf = ""
+
+while True:
+  for byte in bytelist:
+    bus.write_byte(DEVICE_ADDRESS, byte)
+    charBuf = charBuf + chr(bus.read_byte(DEVICE_ADDRESS)) 
+    count = count + 1
+    if count == 5:
+      print charBuf
+      charBuf = ""
+      count = 0
+  time.sleep(.010)
