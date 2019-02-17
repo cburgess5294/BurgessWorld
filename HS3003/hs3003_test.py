c73b5b1d6c51e3cb9919ce8f4c0095ff1c0f5497
Chris Burgess <flamingpinto@gmail.com>
2019-02-17 21:47:59 +0000

    Adding HS300x Examples

---
+++ HS3003/hs3003_test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import smbus
+import time
+
+# create instance of smbus on rpi I2C bus 1 
+bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+
+# Give the I2C device time to settle
+time.sleep(2)
+
+# define sensor i2C address
+DEVICE_ADDRESS = 0x44  #7 bit address (will be left shifted to add the read write bit)
+#DEVICE_REG_MODE1 = 0x00
+#DEVICE_REG_LEDOUT0 = 0x1d
+
+# function to clear bits from returned data bytes
+def clearBit(int_type, offset):
+   mask = ~(1 << offset)
+   return(int_type & mask)
+
+# read sensor
+bus.write_quick(DEVICE_ADDRESS) # wake sensor up and take a reading
+time.sleep(.040) # give the sensor time to gather data
+readSensor =  bus.read_i2c_block_data(DEVICE_ADDRESS,0,4) # read 4 bytes of data you could change the 4 to a 2 in order to only read humidity data
+
+# process humidity data
+hum = readSensor[0] # put first byte of humidity data into a variable
+hum = clearBit( hum , 7 ) # clear bit 7 - this is a status bit
+hum = clearBit( hum, 6 ) # clear bit 6 - this is a status bit
+hum = hum * 256.0 + readSensor[1] # shift first byte left 8 bits and add second byte of humidity data
+hum = (hum / 16383) * 100 # convert raw data to humidity reading
+print "humidity" , int(hum) , "%" # print humidity reading at integer
+
+# process temperature data
+temp = readSensor[2] * 256  # put first byte of temperature data into a variable and shift 8-bits to the left
+temp = temp + readSensor[3] # add first and second bytes of temperature data together
+temp = temp >> 2 # shift temperature data 2 bits to the left to mask undefined bits
+temp = ( temp / 16383.0) * 165 - 40 # convert raw temperature data to celsius 
+print "temperature" , int(temp) , "C" # print temperature as in in C
---
+++ Raspberry Pi Shield for Arduino/examples/python_scrips/rpi_serial_test_b+.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import serial
+import time
+
+port = serial.Serial("/dev/ttyAMA0", baudrate=57600, timeout=3.0)
+print("Raspberry Pi - AVR Serial Test")
+print
+print("Port Information:")
+print("----------------------------------------------------------------")
+print(port)
+print
+print("Send some data from the AVR and the rPI will reply")
+rcv = ""
+port.write("\r\nSay something:")
+while True:
+    time.sleep(.100)
+    byteCount = port.inWaiting()
+    if byteCount  >= 1:
+      rcv = port.read(byteCount)
+      if rcv != "" and rcv != "\n" and rcv != "\r":
+        port.write("\r\nYou sent:" + repr(rcv))
