73e77141c9189b505293b9506120bf8701aebc6f
cburgess5294 <flamingpinto@gmail.com>
2020-12-28 09:06:04 -0500

    modified avr_logic_test python script

--- Raspberry Pi Shield for Arduino/examples/python_scrips/avr_logic_test.py
+++ Raspberry Pi Shield for Arduino/examples/python_scrips/avr_logic_test.py
@@ -12,6 +12,7 @@ GPIO.setup(INPUT_PIN, GPIO.IN)           # Set our input pin to be an input
 GPIO.setup(OUTPUT_PIN, GPIO.OUT)
 GPIO.output(OUTPUT_PIN, GPIO.LOW)
 # Start a loop that never ends
+print "blinking LED on GPIO13 in response to HIGH signal from AVR pin D6"
 while True:
    try: 
            if (GPIO.input(INPUT_PIN) == True): # Physically read the pin now
