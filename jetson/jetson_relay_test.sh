a82204a7e92ab39d66f926b477b46e34b4967828
chris burgess <flamingpinto@gmail.com>
2022-01-12 12:31:22 -0500

    update jetson nano example

--- jetson/jetson_relay_test.sh
+++ jetson/jetson_relay_test.sh
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
-all_pins = (23, 11, 12, 13, 15, 21, 18, 19)
+all_pins = (7, 11, 12, 13, 15, 21, 18, 19)
 GPIO.setmode(GPIO.BOARD)
 
 
@@ -16,9 +16,9 @@ for pin in all_pins:
         while True:
             GPIO.setup(pin, GPIO.OUT)
             GPIO.output(pin, GPIO.LOW)
-            time.sleep(0.55)
+            time.sleep(0.25)
             GPIO.output(pin, GPIO.HIGH)
-            time.sleep(0.55)
+            time.sleep(0.25)
     except KeyboardInterrupt:
         pass
     finally:
