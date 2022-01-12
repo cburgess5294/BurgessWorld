d53e27384636bd5e3ab0d0b809b292ddfb8ff334
cburgess5294 <flamingpinto@gmail.com>
2022-01-12 12:27:18 -0500

    add jetson nano example

---
+++ jetson/jetson_relay_test.sh
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+all_pins = (23, 11, 12, 13, 15, 21, 18, 19)
+GPIO.setmode(GPIO.BOARD)
+
+
+for pin in all_pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+
+for pin in all_pins:
+    GPIO.setmode(GPIO.BOARD)
+    print("Testing pin %d as OUTPUT; CTRL-C to test next pin" % pin)
+    try:
+        while True:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, GPIO.LOW)
+            time.sleep(0.55)
+            GPIO.output(pin, GPIO.HIGH)
+            time.sleep(0.55)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        GPIO.cleanup()
